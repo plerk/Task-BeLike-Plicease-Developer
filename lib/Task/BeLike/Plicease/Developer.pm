cd48b06706e3f296e3ccf9bb683f4fad6d45b9df
Graham Ollis <plicease@cpan.org>
2013-06-08 08:43:36 -0400

    initial implementation

--- lib/Task/BeLike/Plicease/Developer.pm
+++ lib/Task/BeLike/Plicease/Developer.pm
@@ -7,4 +7,13 @@ use v5.10;
 # ABSTRACT: Developer Module Plicease Uses
 # VERSION
 
+=head1 DESCRIPTION
+
+Meta package that requires
+L<Dist::Zilla::PluginBundle::ACPS> and
+L<Dist::Zilla::PluginBundle::Author::Plicease>.
+That's all folks.
+
+=cut
+
 1;
--- t/use.t
+++ t/use.t
@@ -2,4 +2,4 @@ use strict;
 use warnings;
 use Test::More tests => 1;
 
-use_ok 'Task::BeLike-Plicease-Developer';
+use_ok 'Task::BeLike::Plicease::Developer';
